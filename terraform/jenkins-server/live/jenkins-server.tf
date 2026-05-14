9e5aad68ef0e94a79d3664157171c45506a75bbb
Bommxun2 <wachirakorn.cn@gmail.com>
2026-05-14 10:05:01 +0700

    modify v2 jenkins

--- terraform/jenkins-server/live/jenkins-server.tf
+++ terraform/jenkins-server/live/jenkins-server.tf
@@ -6,7 +6,7 @@ terraform {
     }
   }
   backend "s3" {
-    bucket       = "tertis-tf-bucket-1"
+    bucket       = "tertis-tf-bucket"
     region       = "us-east-1"
     key          = "terraform.tfstate"
     encrypt      = true
